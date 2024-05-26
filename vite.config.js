05f5ac6365434f9abfb6bd5294e63b372cba73b3
Copeland Carter <smartycope@gmail.com>
2024-05-25 18:19:17 -0600

    Hopefully fixed some deployment problems

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
     plugins: [react()],
-    base: '/geodoodle/',
+    base: '/',
     server: {
         port: 3006,
     },
