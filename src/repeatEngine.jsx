b7b5d121c4ab88e871980503f9d136187ece484f
Copeland Carter <smartycope@gmail.com>
2025-05-02 16:07:16 -0500

    Major overhaul of the repeat engine

--- src/repeatEngine.jsx
+++ src/repeatEngine.jsx
@@ -1,9 +1,21 @@
 import {MIRROR_AXIS} from "./globals";
-import {getSelected, calc, align} from "./utils";
+import {viewportWidth, viewportHeight, getSelected, calc, align} from "./utils";
 
 // This is the main logic for generating the trellis
 // It returns a list of groups of the pattern with appropriate translations
 export function getTrellis(state){
+    /* Indices
+        rows:
+        -2 <- top row
+        -1
+        0 <- seed pattern
+        1
+        2 <- bottom row
+
+        columns:
+        left   -2 -1 0 1 2   right
+    */
+
     const {
         trellisOverlap,
         trellisSkip,
@@ -14,118 +26,128 @@ export function getTrellis(state){
         debug,
     } = state
 
-    const {boundRect} = calc(state)
+    const {boundRect, scaledTranslationx, scaledTranslationy} = calc(state)
 
-    var rtn = []
-
-    // console.log('getTrellis() called!');
+    var rtn = new Set()
 
     // First, calculate all the variables we need
     // A list of <line> objects, or null
     // Coord: absolute @ (0,0), scaled
     const pattern = getSelected(state)
-    // Coord: scalar, scaled
-    let areaWidth  = window.visualViewport.width  / scalex
-    let areaHeight = window.visualViewport.height / scaley
-    // Coord: scalar, scaled
-    // The extra - width/height is to make sure we're drawing off the page
-    // let startOffsetx = (((boundRect.left * scalex + translationx) % (boundRect.width  * scalex)) / scalex) - boundRect.width
-    // let startOffsety = (((boundRect.top  * scaley + translationy) % (boundRect.height * scaley)) / scaley) - boundRect.height
-    // let startOffsetx = (translationx % scalex + 1) / scalex
-    // let startOffsety = (translationy % scaley + 1) / scaley
-    // let startOffsetx = (translationx / scalex)
-    // let startOffsety = (translationy / scaley)
-    // let startOffsetx = 0
-    // let startOffsety = 0
 
     let width = boundRect.width
     let height = boundRect.height
-    // width += trellisOverlap.col.val.x
-    // width += trellisOverlap.row.val.x
-    // height += trellisOverlap.col.val.y
-    // height += trellisOverlap.row.val.y
-    let startOffsetx = (((boundRect.left * scalex + translationx) % (width  * scalex)) / scalex) - width
-    let startOffsety = (((boundRect.top  * scaley + translationy) % (height * scaley)) / scaley) - height
 
-    if (debug){
-        areaWidth = (window.visualViewport.width / 1.5) / scalex
-        areaHeight = (window.visualViewport.height / 1.5) / scaley
+    // TODO: this could use some refinement, though I think it's pretty solid for now
+    // I think this is accurate?
+    const dimDiff = (trellisFlip.row.val || trellisFlip.col.val || trellisRotate.row.val || trellisRotate.col.val) ? Math.abs(width - height) : 0
+    // Imagine the "default" pattern to be a tellise with no special properties: it's just tesselated evenly across the area
+    // This is at most how many dots outside a pattern might go outside of it's "default" position
+    const retrogradex_dots = trellisOverlap.row.val.x + trellisOverlap.col.val.x + dimDiff + height + 1
+    const retrogradey_dots = trellisOverlap.row.val.y + trellisOverlap.col.val.y + dimDiff + width + 1
 
-        const debugBox_xy = align(state, window.visualViewport.width / 4, window.visualViewport.height / 4)
-        // areaWidth = (window.visualViewport.width / 2) / scalex
-        // areaHeight = (window.visualViewport.height / 2) / scaley
-        startOffsetx += debugBox_xy[0] / scalex
-        startOffsety += debugBox_xy[1] / scaley
-    }
+    function applyTransformations(x, y, row, col){
+        // Skip
+        if ((!trellisSkip.row.val || !(col % trellisSkip.row.every)) &&
+            (!trellisSkip.col.val || !(row % trellisSkip.col.every))){
+        
+            // Initial translation
+            let transformation = `translate(${x}, ${y})`
+            // let transformation = ''
 
-    // console.warn('area:', areaWidth, areaHeight);
-    // console.log('shift:', shiftx, shifty);
-    // console.log('boundRect:', boundRect);
-    // console.log('trellisOverlap:', trellisOverlap);
-    // console.log('lhs:', ((boundRect.left * scalex) - translationx) / scalex);
-    // console.log('rhs:', (boundRect.width  * scalex) / scalex);
-    // console.log('startOffset:', startOffsetx, startOffsety);
-    // console.log('bounds:', bounds);
-    // console.log('pattern:', pattern);
+            // Offset/Overlap
+            if (!(col % trellisOverlap.row.every) && trellisOverlap.row.val.x && trellisOverlap.row.val.y)
+                transformation += `translate(${trellisOverlap.row.val.x}, ${trellisOverlap.row.val.y})`
+            if (!(row % trellisOverlap.col.every) && trellisOverlap.col.val.x && trellisOverlap.col.val.y)
+                transformation += `translate(${trellisOverlap.col.val.x}, ${trellisOverlap.col.val.y})`
 
-    // Checking that we don't try to do something that ruins our math (i.e. /0)
-    if (pattern.length < 1 || boundRect.width < 1 || boundRect.height < 1)
-        return []
+            // Rotate
+            if (!(col % trellisRotate.row.every) && trellisRotate.row.val)
+                transformation += `rotate(${trellisRotate.row.val})`
+            if (!(row % trellisRotate.col.every) && trellisRotate.col.val)
+                transformation += `rotate(${trellisRotate.col.val})`
 
-
-        for (let row = 0, x = startOffsetx; x < areaWidth; x += boundRect.width, row++) {
-        for (let col = 0, y = startOffsety; y < areaHeight; y += boundRect.height, col++) {
-            // Skip
-            if ((!trellisSkip.row.val || !(col % trellisSkip.row.every)) &&
-                (!trellisSkip.col.val || !(row % trellisSkip.col.every))){
-
-                // Initial translation
-                let transformation = `translate(${x}, ${y}) `
-
-                // Offset/Overlap
-                if (!(col % trellisOverlap.row.every))
-                    transformation += `translate(${trellisOverlap.row.val.x}, ${trellisOverlap.row.val.y})`
-                if (!(row % trellisOverlap.col.every))
-                    transformation += `translate(${trellisOverlap.col.val.x}, ${trellisOverlap.col.val.y})`
-
-                // Rotate
-                if (!(col % trellisRotate.row.every))
-                    transformation += `rotate(${trellisRotate.row.val})`
-                if (!(row % trellisRotate.col.every))
-                    transformation += `rotate(${trellisRotate.col.val})`
-
-                // Flip
-                if (!(col % trellisFlip.row.every)){
-                    if (trellisFlip.row.val === MIRROR_AXIS.VERT_90 || trellisFlip.row.val === MIRROR_AXIS.BOTH_360)
-                        transformation += `matrix(-1, 0, 0, 1, 0, 0) `
-                    if (trellisFlip.row.val === MIRROR_AXIS.HORZ_180 || trellisFlip.row.val === MIRROR_AXIS.BOTH_360)
-                        transformation += `matrix(1, 0, 0, -1, 0, 0) `
-                }
-                if (!(row % trellisFlip.col.every)){
-                    if (trellisFlip.col.val === MIRROR_AXIS.VERT_90 || trellisFlip.col.val === MIRROR_AXIS.BOTH_360)
-                        transformation += `matrix(-1, 0, 0, 1, 0, 0) `
-                    if (trellisFlip.col.val === MIRROR_AXIS.HORZ_180 || trellisFlip.col.val === MIRROR_AXIS.BOTH_360)
-                        transformation += `matrix(1, 0, 0, -1, 0, 0) `
-                }
-
-                // if ((areaWidth/2) / boundRect.width === x && (areaHeight/2) / boundRect.height === y)
-                //     center = pattern
-
-                const selected = (row === 4 && col === 4)
-                    // (x === ((areaWidth-startOffsetx) / 2) / boundRect.width) &&
-                    // (y === ((areaHeight-startOffsety) / 2) / boundRect.height)
-
-
-                rtn.push(<g
-                    transform={transformation}
-                    key={`${row}-${col}`}
-                    id={selected ? 'selected-trellis-pattern' : undefined}
-                >
-                    {pattern}
-                </g>)
+            // Flip
+            if (!(col % trellisFlip.row.every)){
+                if (trellisFlip.row.val === MIRROR_AXIS.VERT_90 || trellisFlip.row.val === MIRROR_AXIS.BOTH_360)
+                    transformation += `matrix(-1, 0, 0, 1, 0, 0)`
+                if (trellisFlip.row.val === MIRROR_AXIS.HORZ_180 || trellisFlip.row.val === MIRROR_AXIS.BOTH_360)
+                    transformation += `matrix(1, 0, 0, -1, 0, 0)`
             }
+            if (!(row % trellisFlip.col.every)){
+                if (trellisFlip.col.val === MIRROR_AXIS.VERT_90 || trellisFlip.col.val === MIRROR_AXIS.BOTH_360)
+                    transformation += `matrix(-1, 0, 0, 1, 0, 0)`
+                if (trellisFlip.col.val === MIRROR_AXIS.HORZ_180 || trellisFlip.col.val === MIRROR_AXIS.BOTH_360)
+                    transformation += `matrix(1, 0, 0, -1, 0, 0)`
+            }
+
+            return transformation
+        }
+        else{
+            return null
         }
     }
 
-    return rtn
+    // TODO: add a function which checks if the transformation would place it outside the viewport, and skip it if it would
+    // we still need to go out a certain retrograde amount, because if we offset a skipped row/col outside of the viewport 
+    // back inside it
+    // This will require calculating the correct translation for each pattern instead of just using 'translate()' from the 
+    // default position, which is substantially more difficult.
+    function addPattern(transformation){
+        if (transformation)
+            rtn.add(transformation)
+    }
+
+    // Coords: absolute, scaled
+    // The base pattern location
+    const seedx = boundRect.left + scaledTranslationx 
+    const seedy = boundRect.top + scaledTranslationy
+
+    // Coords: realtive, scaled
+    let debugBox_xy = align(state, viewportWidth() / 4, viewportHeight() / 4)
+    debugBox_xy = [debugBox_xy[0] / scalex, debugBox_xy[1] / scaley]
+
+    const debug_shrink_factor = debug ? 2 : 1
+    const areaWidth = (viewportWidth() / debug_shrink_factor) / scalex
+    const areaHeight = (viewportHeight() / debug_shrink_factor) / scaley
+    const areaRect = {
+        left: debug ? debugBox_xy[0] : 0,
+        top: debug ? debugBox_xy[1] : 0,
+        right: debug ? debugBox_xy[0] + areaWidth : areaWidth,
+        bottom: debug ? debugBox_xy[1] + areaHeight : areaHeight,
+    }
+
+    // Instead of gracefully trying to calculate what will be needed, instead add all the transformed patterns one at a time
+    // First, start at the seed pattern, and add patterns one at a time in each direction until the bounds of the pattern are entirely 
+    // outside the area for retrograde* number of patterns
+    // That essentially gives us the axes. From there, just fill in each quadrant
+
+    // Top left quadrant
+    // This one adds the up and left axis columns
+    for (let posx = seedx, col = 0; posx > areaRect.left - retrogradex_dots; posx -= width, col--){
+        for (let posy = seedy, row = 0; posy > areaRect.top - retrogradey_dots; posy -= height, row--)
+            addPattern(applyTransformations(posx, posy, row, col))
+    }
+    // Top right quadrant
+    for (let posx = seedx + width, col = 1; posx < areaRect.right + retrogradex_dots; posx += width, col++){
+        for (let posy = seedy, row = 0; posy > areaRect.top - retrogradey_dots; posy -= height, row--)
+            addPattern(applyTransformations(posx, posy, row, col))
+    }
+    // Bottom left quadrant
+    for (let posx = seedx + width, col = 1; posx > areaRect.left - retrogradex_dots; posx -= width, col--){
+        for (let posy = seedy + height, row = 1; posy < areaRect.bottom + retrogradey_dots; posy += height, row++)
+            addPattern(applyTransformations(posx, posy, row, col))
+    }
+    // Bottom right quadrant
+    // This one adds the down and right axis columns
+    for (let posx = seedx, col = 0; posx < areaRect.right + retrogradex_dots; posx += width, col++){
+        for (let posy = seedy, row = 0; posy < areaRect.bottom + retrogradey_dots; posy += height, row++)
+            addPattern(applyTransformations(posx, posy, row, col))
+    }
+    
+    return Array.from(rtn).map(transformation => <g
+        transform={transformation}
+        // This seems inneficient, but I can't figure out why it wouldn't work...
+        key={transformation}
+    > {pattern} </g>)
 }
